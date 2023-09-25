5d69996f9aad75ebdeb8f8370832d503dd8f5e40
Julio-Mugnol <jcmugnol@gmail.com>
2023-09-25 14:03:09 -0300

    Exercicio 3, script fix

--- src/3.R
+++ src/3.R
@@ -8,28 +8,27 @@ itpd <- read_dta("dados/itpd.dta")
 itpd.a <- itpd %>% 
   filter(importer_iso3 != exporter_iso3) %>% 
   group_by(exporter_iso3, year, industry_id) %>% 
-  summarise(total_industria = sum(trade / 1000))
+  summarise(total_pais_industria = sum(trade / 1000))
 
 ## B) ----
 itpd.b <- itpd.a %>% 
   left_join(itpd.a %>%
               group_by(exporter_iso3, year) %>% 
-              summarise(total_trade = sum(total_industria)),
+              summarise(total_pais = sum(total_pais_industria)),
             by = c("exporter_iso3", "year")) %>% 
-  mutate(vantagem_absoluta = total_industria / total_trade)
+  mutate(vantagem_absoluta = total_pais_industria / total_pais)
 
 ## C) ----
 itpd.c <- itpd.b %>% 
   left_join(itpd.b %>%
               group_by(year) %>% 
-              summarise(total_trade_ano = sum(total_industria)),
+              summarise(total_ano = sum(total_pais_industria)),
             by = c("year")) %>% 
   left_join(itpd.b %>%
               group_by(industry_id, year) %>% 
-              summarise(total_trade_industria = sum(total_industria)),
+              summarise(total_industria = sum(total_pais_industria)),
             by = c("industry_id", "year")) %>% 
-  mutate(RCA = vantagem_absoluta / (total_trade_industria / total_trade_ano)) %>% 
-  select(exporter_iso3, year, industry_id, RCA)
+  mutate(RCA = vantagem_absoluta / (total_industria / total_ano))
 
 ## Salvar base
 itpd.c %>% 
@@ -37,6 +36,7 @@ itpd.c %>%
               as_tibble() %>% 
               mutate(id = seq(1, get_labels(itpd.c$industry_id) %>% length(), 1)),
             by = c("industry_id" = "id")) %>% 
-  select(exporter_iso3, year, RCA, industry_name = value) %>% 
+  select(exporter_iso3, year, total_pais_industria, total_pais, total_industria, 
+         total_ano, RCA, industry_name = value) %>% 
   write.csv(., file = "relatorios/tabela_ex3.csv")
 
